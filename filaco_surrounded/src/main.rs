6f0734ab51a906375b455ff300ca933b12e56a84
FLC <awesome.chekushka@gmail.com>
2023-12-22 03:22:59 +0500

    feat(base): add base components

--- filaco_surrounded/src/main.rs
+++ filaco_surrounded/src/main.rs
@@ -1,6 +1,32 @@
 use bevy::prelude::*;
-use filaco_surrounded::feature::SurroundedPlugin;
+use bevy_rapier3d::prelude::*;
+use std::collections::HashMap;
 
 fn main() {
-    App::new().add_plugins(SurroundedPlugin).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .run()
+}
+
+fn handle_input(scan_codes: Res<Input<ScanCode>>) {}
+
+#[derive(Component)]
+struct Stranger;
+git
+#[derive(Component)]
+struct InGameCamera;
+
+#[derive(Component)]
+struct MenuCamera;
+
+struct GameState {
+    stage: PlotStage,
+}
+
+enum PlotStage {
+    Childhood,
+    Adolescence,
+    Youth,
+    Final,
 }
